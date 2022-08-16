4529e52cfa0c619f1c23d38d9a20e0a4d4dbcb74
Alone-kurd <sevra650@gmail.com>
2022-08-16 16:24:57 +0300

    seede

---
+++ pages/alone.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>video star</title>
+</head>
+<body>
+  <div class="page">
+    <div class="navbar">
+      <div class="navbar-inner sliding">
+        <div class="left">
+          <a href="#" class="link back"><span class=
+          "ios-only">Back</span></a>
+        </div>
+        <div class="title">
+          VideoStar pro
+        </div>
+        <div class="right">
+          <a href="javascript:window.location.reload(true)" class=
+          "f7-icons">reload</a>
+        </div>
+      </div>
+    </div>
+    <div class="page-content">
+      <div class="list media-list inset elevation-12">
+        <ul>
+          <li class="media-item">
+            <div class="item-content">
+              <div class="item-media"><img src=
+              "./img/videostar.png"
+              class="appicon elevation-5 animated zoomIn"></div>
+              <div class="item-inner">
+                <div class="item-title-row">
+                  <div class="item-title">
+                    VideoStar pro
+                  </div><a href="itms-services://?action=download-manifest&url=https://alone-ios.netlify.app/install/videostar.plist"
+                  class=
+                  "button button-round button-fill button-raised color-green external">Get
+                  <i class=
+                  "icon f7-icons color-white">cloud_download</i></a>
+                </div>
+                <div class="item-subtitle">
+                  Hacked
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="block-title">
+        About videostar pro 2
+      </div>
+      <div class="block block-strong inset elevation-12">
+        <pre>Hacked - All Features Unlocked</pre>
+      </div>
+      <div class="list inset elevation-12">
+        <ul>
+          <li>
+            <div class="item-content">
+              <div class="item-inner">
+                <div class="item-title">
+                  Version
+                </div>
+                <div class="item-after">
+                  v8.9
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="item-content">
+              <div class="item-inner">
+                <div class="item-title">
+                  Size
+                </div>
+                <div class="item-after">
+                  139MB
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
