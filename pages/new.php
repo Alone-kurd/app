1fc43e13f87a99c2f83bb041555106bf54b98dd9
alone-kurd <sevar650@gmail.com>
2023-07-28 15:53:59 +0300

    alone is back

--- pages/new.php
+++ pages/new.php
@@ -65,73 +65,38 @@
                                         </div>
                                     </a>
                                 </li>
-          <li class="media-item">
+                            <li class="media-item">
+                              <a href="/videostar" class="item-link">
+                                <div class="item-content">
+                                  <div class="item-media">
+                                    <img src="img/videostar.png" class="appiconx elevation-5 animated zoomIn"></div>
+                                  <div class="item-inner">
+                                    <div class="item-title-row">
+                                      <div class="item-title">VideoStar Hack</div>
+                                    </div>
+                                    <div class="chip">
+                                      <div class="chip-media bg-color-blue"><i class="icon f7-icons">gear</i></div>
+                                      <div class="chip-label">8.9</div>
+                                    </div>
+                                    <div class="chip">
+                                      <div class="chip-media bg-color-blue"><i class="icon f7-icons">download</i></div>
+                                      <div class="chip-label">139MB</div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </a>
+                            </li>
                                 <li class="media-item">
-                                    <a href="/ppsspp" class="item-link">
+                                    <a href="/vdhack" class="item-link">
                                         <div class="item-content">
                                             <div class="item-media">
-                                                <img src="img/ppsspp.png" class="appiconx elevation-5 animated zoomIn"></div>
+                                                <img src="img/vdhack.png" class="appiconx elevation-5 animated zoomIn"></div>
                                             <div class="item-inner">
                                                 <div class="item-title-row">
-                                                    <div class="item-title">PPSSPP</div>
+                                                    <div class="item-title">Video StarHack</div>
                                                 </div>
                                                 <div class="chip">
                                                     <div class="chip-media bg-color-blue"><i class="icon f7-icons">gear</i></div>
-                                                    <div class="chip-label">1.10.3-1201</div>
+                                                    <div class="chip-label">vlast</div>
                                                 </div>
-                                                <div class="chip">
-                                                    <div class="chip-media bg-color-blue"><i class="icon f7-icons">download</i></div>
-                                                    <div class="chip-label">16.53 MB</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                                <li class="media-item">
-                                    <a href="/bolt" class="item-link">
-                                        <div class="item-content">
-                                            <div class="item-media">
-                                                <img src="img/bolt.png" class="appiconx elevation-5 animated zoomIn"></div>
-                                            <div class="item-inner">
-                                                <div class="item-title-row">
-                                                    <div class="item-title">Bolt</div>
-                                                </div>
-                                                <div class="chip">
-                                                    <div class="chip-media bg-color-blue"><i class="icon f7-icons">gear</i></div>
-                                                    <div class="chip-label">1.8</div>
-                                                </div>
-                                                <div class="chip">
-                                                    <div class="chip-media bg-color-blue"><i class="icon f7-icons">download</i></div>
-                                                    <div class="chip-label">15.12 MB</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                                <li class="media-item">
-                                    <a href="/procam" class="item-link">
-                                        <div class="item-content">
-                                            <div class="item-media">
-                                                <img src="img/procam.png" class="appiconx elevation-5 animated zoomIn"></div>
-                                            <div class="item-inner">
-                                                <div class="item-title-row">
-                                                    <div class="item-title">ProCam</div>
-                                                </div>
-                                                <div class="chip">
-                                                    <div class="chip-media bg-color-blue"><i class="icon f7-icons">gear</i></div>
-                                                    <div class="chip-label">13.6.6</div>
-                                                </div>
-                                                <div class="chip">
-                                                    <div class="chip-media bg-color-blue"><i class="icon f7-icons">download</i></div>
-                                                    <div class="chip-label">23.9 MB</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</body>
-</html>
+                                                
